42acc1fa37514cd4ddb61ae673f308794e59db06
Eren <erencemayez@gmail.com>
2026-02-25 00:51:34 +0300

    delete legacy src/ codebase

--- src/state/app-state.ts
+++
@@ -1,7 +0,0 @@
-import type { UiState } from "./types";
-
-export class AppStateStore implements UiState {
-  public focusMode: UiState["focusMode"] = "code";
-  public viewMode: UiState["viewMode"] = "code";
-  public selectedChipIndex = 0;
-}
